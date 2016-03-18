970477a8c1bacd2acc148312e3f4bed1f99e264c
墨染 <184918211@qq.com>
2016-03-18 21:17:23 +0800

    初具原型

---
+++ app.js
@@ -0,0 +1,24 @@
+var http=require("http");
+var fs=require("fs");
+var url=require("url");
+var users=[];
+http.createServer(function(req,res){
+    var urlObj=url.parse(req.url);
+    if(urlObj.path=='/'){
+        fs.readFile('html/reg/index.html','utf8', function (err,data) {
+            res.end(data)
+        })
+    }else if(urlObj.path=='/reg'){
+        var str=[];
+        req.on('data',function(data){
+            str.push(data)
+        });
+        req.on('end', function () {
+            str=Buffer.concat(str).toString();
+            str&&users.push(JSON.parse(str));
+            res.end(JSON.stringify(users));
+        })
+    }else {
+        res.end("404")
+    }
+}).listen(8080);
